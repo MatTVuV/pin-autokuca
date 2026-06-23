95c6d58a4af9e750f6e99e558a24a83e545f5af7
Matija <matija.tausan@vuv.hr>
2026-06-23 18:42:17 +0200

    Dodan EnsureDeleted

--- PINAutoKucaBackend/Program.cs
+++ PINAutoKucaBackend/Program.cs
@@ -88,6 +88,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
 }
 
