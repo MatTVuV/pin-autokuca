e797b768c46b6e9b49f04489a92c04548095b7cd
Matija <matija.tausan@vuv.hr>
2026-06-23 19:52:35 +0200

    Popravljene migracije2

--- PINAutoKucaBackend/Program.cs
+++ PINAutoKucaBackend/Program.cs
@@ -88,8 +88,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 app.Run();
